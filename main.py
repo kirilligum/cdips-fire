10474301a2d0d13d29da152856ae1c46b4494305
Kirill Igumenshchev <kirilligum@gmail.com>
2014-07-25 09:20:16 -0700

    added first letter of non-unique hierarchical label to onehot

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ data  = pd.read_csv('train.csv',nrows=10000)
 td = onehot.transform(data,['var1','var2','var3','var4'])
 
 print td.shape
-print td[0:10].toarray()
+print td[0:2].toarray()
--- onehot.py
+++ onehot.py
@@ -2,10 +2,10 @@
 
 from sklearn.preprocessing import OneHotEncoder
 
-def col_to_list(d,v):
-    dv= d.ix[:,v]
+def col_to_list(dv):
     enum = list(set(dv))
-    enum.insert(len(enum),enum.pop(enum.index('Z'))) #move Z (missing value) to be the last element
+    if enum.count('Z'):
+        enum.insert(len(enum),enum.pop(enum.index('Z'))) #move Z (missing value) to be the last element
     for x in range(0,len(dv)):
         for i in range(0,len(enum)):
             if dv[x] == enum[i]:
@@ -13,15 +13,17 @@ def col_to_list(d,v):
     return dv
 
 
-def transform(data,vars):
+def transform(data,vars): # takes pandas.read_csv and list of strings of variables, and returns onehot matrix
     d =[]
     for i in vars:
-        d.append(col_to_list(data,i))
+        dv= data.ix[:,i]
+        if i=='var4': # taking care of categories by making another label of just the letter (label[0])
+            cdv = []
+            cdv = [x[0] for x in list(set(dv)) if len(x)>1 and int(x[1])>1] # truncating to the first letter and remove letters with only one number child
+            d.append(col_to_list(cdv))
+        d.append(col_to_list(dv))
     enc = OneHotEncoder()
     enc.fit(zip(*d))
-    #print enc.active_features_
-    #print enc.feature_indices_
-    #print enc.n_values_
     td = enc.transform(zip(*d))
     return td
 
