fb0974ad8a8762dc4178d7333ec444b14619570a
Kirill Igumenshchev <kirill@berkeley.edu>
2014-07-30 15:40:27 -0700

    added  backward compability to onehot

--- onehot.py
+++ onehot.py
@@ -3,6 +3,35 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
+def col_to_list(dv):
+    rdv = list(dv)
+    enum = list(set(dv))
+    enum.sort() #set is unordered; could lead to problems of column mismatch later, so best to sort....
+#     if enum.count('Z'):
+#         enum.insert(len(enum),enum.pop(enum.index('Z'))) #move Z (missing value) to be the last element
+    for x in range(0,len(dv)):
+        for i in range(0,len(enum)):
+            if dv[x] == enum[i]:
+                rdv[x] = i;
+    return rdv
+
+
+def transform(data,cols): # takes pandas.read_csv and list of strings of variables, and returns onehot matrix
+    d =[]
+    for i in cols:
+        dv= data.ix[:,i]
+        if i=='var4': # taking care of categories by making another label of just the letter (label[0])
+            cdv = []
+            parents = [x[0] for x in list(set(dv)) if len(x)>1 and int(x[1])==2] # truncating to the first letter and remove letters with only one number child
+            parents.sort()
+            cdv = [x[0] if (parents.count(x[0])) else 0 for x in dv] # truncating to the first letter and remove letters with only one number child
+            d.append(col_to_list(cdv))
+        d.append(col_to_list(dv))
+    enc = OneHotEncoder()
+    enc.fit(zip(*d))
+    td = enc.transform(zip(*d))
+    return td
+
 class onehot:
     def __init__(self):
         self.enc = OneHotEncoder()
