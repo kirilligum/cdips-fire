669836963f7a8eedd44c7d7d91244674e58547df
Kirill Igumenshchev <kirill@berkeley.edu>
2014-08-31 23:09:05 -0700

    multi-model mvp worksvim main.py

--- ohmbcr/main.py
+++ ohmbcr/main.py
@@ -45,50 +45,56 @@ start = time.clock()
 ###############################################################
 ###### begin model
 
+###### do classification
+rfc= RandomForestClassifier(n_jobs=-1)
+#rfc= RandomForestClassifier(n_estimators = 1000,n_jobs=-1)
+rfc = rfc.fit(data_train,target_train)
+predict_loc_class = pd.DataFrame({"target":rfc.predict(data_test), "id": data_test['id']})
+nz_target_test,nz_data_test = get_nz(predict_loc_class['target'],data_test)
+###### do multimodel regression
+## stacking regressor
+strf = RandomForestRegressor()
 ## make a list of models
-rf = RandomForestRegressor(n_jobs=-1)
+rf = RandomForestRegressor()
+#rf = RandomForestRegressor(n_jobs=-1)
 svm = svm.SVR()
 models = [rf,svm]
 
 #### cv train of stackers
-#kf = KFold(5, n_folds=2,shuffle=True)
-kf = KFold(len(nz_data_train), n_folds=10,shuffle=True)
-kfold_data_train = []
-kfold_target_train = []
-kfold_data_test = []
-kfold_target_test = []
-for train, test in kf:
-    #print("%s %s" % (train, test))
-    #print nz_data_train.shape, nz_data_train.iloc[train].shape,"  -  ",
-    #print nz_target_train.shape, nz_target_train.iloc[train].shape
-    kfold_data_train += [nz_data_train.iloc[train]]
-    kfold_target_train += [nz_target_train.iloc[train]]
-    kfold_data_test += [nz_data_train.iloc[test]]
-    kfold_target_test += [nz_target_train.iloc[test]]
-#for i in range(len(kfold_data_train)):
-    #print kfold_data_train[i].shape,kfold_target_train[i].shape, kfold_data_test[i].shape, kfold_target_test[i].shape
-#print kfold_data_train[0].shape
-#print kfold_target_train[0].shape
-#print kfold_data_train[1].shape
-#print kfold_target_train[1].shape
 ## create folds
+kf = KFold(len(nz_data_train), n_folds=10,shuffle=True)
+for train, test in kf:
 ## fit models
+    for i in models:
+        i.fit(nz_data_train.iloc[train],nz_target_train.iloc[train])
 ## predict
+    stpredict = []
+    for i in models:
+        stpredict += [ pd.Series(i.predict(nz_data_train.iloc[test]))]
+    dfstpredict = pd.concat(stpredict,axis=1)
 ## fit stackers
+    strf.fit(dfstpredict,nz_target_train.iloc[test])
 
 #### stack
 ## fit all models
+for i in models:
+    i.fit(nz_data_train,nz_target_train)
+#print models
 ## predict from all models
+stpredict = []
+for i in models:
+    stpredict += [ pd.Series(i.predict(nz_data_test))]
+dfstpredict = pd.concat(stpredict,axis=1)
 ## predict and average stackers
+print dfstpredict
+print nz_data_test.shape
+print dfstpredict.mean(axis=1)
+stack_predicted = strf.predict(dfstpredict)
+print stack_predicted
 
-rfc= RandomForestClassifier(n_jobs=-1)
 rfr = RandomForestRegressor(n_jobs=-1)
-#rfc= RandomForestClassifier(n_estimators = 1000,n_jobs=-1)
 #rfr= RandomForestRegressor(n_estimators = 1000,n_jobs=-1)
-rfc = rfc.fit(data_train,target_train)
 rfr = rfr.fit(nz_data_train,nz_target_train)
-predict_loc_class = pd.DataFrame({"target":rfc.predict(data_test), "id": data_test['id']})
-nz_target_test,nz_data_test = get_nz(predict_loc_class['target'],data_test)
 predict_loc_regres = pd.DataFrame({"target":rfr.predict(nz_data_test), "id": nz_data_test['id']})
 
 ###### end model
