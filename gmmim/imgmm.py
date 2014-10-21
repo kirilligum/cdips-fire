41eb2cad3b696cf6008b8e27789099f77ed09aed
Kirill Igumenshchev <kirill@berkeley.edu>
2014-10-20 21:20:25 -0700

    gmm imputation  does predictions but doesn't save the sample yet

--- gmmim/imgmm.py
+++ gmmim/imgmm.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import scipy
 from sklearn.preprocessing import Imputer
 from sklearn import mixture
 import time
@@ -7,9 +8,6 @@ import sys
 import os
 import random
 import itertools
-
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 log = open(os.path.splitext(sys.argv[0])[0]+'.log','wt')
 
@@ -24,24 +22,26 @@ data_imp_mean = pd.DataFrame(imp_mean.fit_transform(data),columns=data.columns)
 print data_imp_mean.shape, data.shape
 #print >> log, data_imp_mean, data
 
-g = mixture.GMM(n_components=5, covariance_type='full')
+g = mixture.GMM(n_components=1, min_covar=0.1,thresh=0.001)
+#g = mixture.GMM(n_components=1, covariance_type='full',min_covar=0.5)
 g.fit(data_imp_mean)
 print 'weights_ = ',g.weights_.shape
+print 'means_ = ',g.means_.shape,type(g.means_)
+print 'covars_ = ',g.covars_.shape,type(g.covars_)
 print >> log, 'weights_ = ',g.weights_.shape
 print >> log, 'weights_ = ',g.weights_
 #print 'means_ = ',g.means_.shape
-#print >> log, 'means_ = ',g.means_.shape
-#print >> log, 'means_ = ',g.means_
+print >> log, 'means_ = ',g.means_.shape
+print >> log, 'means_ = ',g.means_
 #print 'covars_ = ',g.covars_.shape
-#print >> log, 'covars_ = ',g.covars_.shape
-#print >> log, 'covars_ = ',g.covars_
+print >> log, 'covars_ = ',g.covars_.shape
+print >> log, 'covars_ = ',g.covars_
 #print 'converged_ = ',g.converged_
-#print >> log, 'converged_ = ',g.converged_
+print >> log, 'converged_ = ',g.converged_
 
 ### Decide from which gaussian to sample by sampling from weights
 decide_node_sample = random.random()
-print decide_node_sample
-print g.weights_
+print decide_node_sample , g.weights_
 iweight = 0
 running_sum = 0
 while decide_node_sample>running_sum:
@@ -50,20 +50,51 @@ while decide_node_sample>running_sum:
   print "running_sum = ", running_sum
 iweight-=1
 print iweight
+print 'means_ = ',g.means_[iweight][:50]
 
 ### separate the vector into known and unknown.
 
-impute_sample = data.iloc[0]
-nan_means = []
-nan_covars = []
-for i,imean, icovar in itertools.zip(impute_sample,g.means_,g.covars_):
-  if np.isnan(i):
-    nan_means += imean
-    nan_covars += icovar
+imps=[]
+prefs =[]
+for isodx, impute_sample in data.ix[:,:].iterrows():
+  nan_means = []
+  nan_covars = []
+  val_means = []
+  val_covars = []
+  impute_sample_nan_idxs = []
+  impute_sample_num_idxs = []
+  for idx,i in enumerate(impute_sample):
+    if np.isnan(i):
+      impute_sample_nan_idxs+=[idx]
+    else:
+      impute_sample_num_idxs+=[idx]
+  print >>log,impute_sample_nan_idxs
+  #print impute_sample_nan_idxs ,impute_sample_num_idxs,data.columns[impute_sample_nan_idxs]
+  #print impute_sample_num_idxs, impute_sample[impute_sample_num_idxs]
+  print >>log,impute_sample_num_idxs
+  #impran= np.random.multivariate_normal(g.means_[iweight][impute_sample_nan_idxs],g.covars_[iweight][np.ix_(impute_sample_nan_idxs,impute_sample_nan_idxs)])
+  if impute_sample_nan_idxs:
+    impran= scipy.stats.multivariate_normal.rvs(g.means_[iweight][impute_sample_nan_idxs],g.covars_[iweight][impute_sample_nan_idxs]*np.eye(len(impute_sample_nan_idxs)))
+    #impran= scipy.stats.multivariate_normal.rvs(g.means_[iweight][impute_sample_nan_idxs],g.covars_[iweight][np.ix_(impute_sample_nan_idxs,impute_sample_nan_idxs)])
   else:
-    prefactor= gaussian(imean,icov)
-impran= np.random.multivariate_normal(nan_means,nan_cavs)
+    impran=1
+  #print impran
+  #print >> log, "pref norm =", g.means_[iweight][impute_sample_num_idxs]
+  #print >> log, "pref cov =",g.covars_[iweight][impute_sample_num_idxs]
+  #print >> log, "pref cov =",g.covars_[iweight][np.ix_(impute_sample_num_idxs,impute_sample_num_idxs)]
+  #print "num =",impute_sample[impute_sample_num_idxs]
+  #print "means = ",g.means_[iweight][impute_sample_num_idxs]
+  #print "covs = ",g.covars_[iweight][np.ix_(impute_sample_num_idxs,impute_sample_num_idxs)]
+  #pref = scipy.stats.multivariate_normal.pdf(impute_sample[impute_sample_num_idxs],g.means_[iweight][impute_sample_num_idxs],g.covars_[iweight][impute_sample_num_idxs]*np.eye(len(impute_sample_num_idxs)))
+  #prefs+=[pref]
+  #print pref
+  #impran*= pref
+  imps+=[impran]
+  print >>log,isodx," imputed = ",impran
+#print >>log,"sorted(pref): ",np.sort(prefs)
+#print "sort(pref): ",np.sort(prefs)[-10:]
 
+#print imps
 
 
 
@@ -71,8 +102,6 @@ impran= np.random.multivariate_normal(nan_means,nan_cavs)
 
 ## sample multivariate normal with uknown.
 
-random_sample = np.random.multivariate_normal(g.means_[0],g.covars_[0])
-print random_sample.shape
 
 #tcols = {}
 #nans ={}
