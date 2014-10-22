7a26339496a413a1c68a903751947fcc1ce90af6
Kirill Igumenshchev <kirill@berkeley.edu>
2014-10-21 17:05:05 -0700

    loop over gmm.fit() but number of nans is error

--- gmmim/imgmm.py
+++ gmmim/imgmm.py
@@ -18,118 +18,57 @@ print >> log, "data.shape = " , data.shape
 miss = data.apply(lambda x : x.apply(lambda y: np.isnan(y)))
 
 imp_mean = Imputer(missing_values=np.nan, strategy='mean', axis=0)
-data_imp_mean = pd.DataFrame(imp_mean.fit_transform(data),columns=data.columns)
-print data_imp_mean.shape, data.shape
-#print >> log, data_imp_mean, data
+fit_data = pd.DataFrame(imp_mean.fit_transform(data),columns=data.columns)
+print fit_data.shape, data.shape
+#print >> log, fit_data, data
+#print fit_data.ix[:0,:5]
 
-g = mixture.GMM(n_components=1, min_covar=0.1,thresh=0.001)
-#g = mixture.GMM(n_components=1, covariance_type='full',min_covar=0.5)
-g.fit(data_imp_mean)
-print 'weights_ = ',g.weights_.shape
-print 'means_ = ',g.means_.shape,type(g.means_)
-print 'covars_ = ',g.covars_.shape,type(g.covars_)
-print >> log, 'weights_ = ',g.weights_.shape
-print >> log, 'weights_ = ',g.weights_
-#print 'means_ = ',g.means_.shape
-print >> log, 'means_ = ',g.means_.shape
-print >> log, 'means_ = ',g.means_
-#print 'covars_ = ',g.covars_.shape
-print >> log, 'covars_ = ',g.covars_.shape
-print >> log, 'covars_ = ',g.covars_
-#print 'converged_ = ',g.converged_
-print >> log, 'converged_ = ',g.converged_
+nan_not_counted = True
+nans_and_nums=[]
+g = mixture.GMM(n_components=10, min_covar=0.001)
 
-### Decide from which gaussian to sample by sampling from weights
-decide_node_sample = random.random()
-print decide_node_sample , g.weights_
-iweight = 0
-running_sum = 0
-while decide_node_sample>running_sum:
-  running_sum+=g.weights_[iweight]
-  iweight+=1
-  print "running_sum = ", running_sum
-iweight-=1
-print iweight
-print 'means_ = ',g.means_[iweight][:50]
-
-### separate the vector into known and unknown.
-
-imps=[]
-prefs =[]
-for isodx, impute_sample in data.ix[:,:].iterrows():
-  nan_means = []
-  nan_covars = []
-  val_means = []
-  val_covars = []
-  impute_sample_nan_idxs = []
-  impute_sample_num_idxs = []
-  for idx,i in enumerate(impute_sample):
-    if np.isnan(i):
-      impute_sample_nan_idxs+=[idx]
-    else:
-      impute_sample_num_idxs+=[idx]
-  print >>log,impute_sample_nan_idxs
-  #print impute_sample_nan_idxs ,impute_sample_num_idxs,data.columns[impute_sample_nan_idxs]
-  #print impute_sample_num_idxs, impute_sample[impute_sample_num_idxs]
-  print >>log,impute_sample_num_idxs
-  #impran= np.random.multivariate_normal(g.means_[iweight][impute_sample_nan_idxs],g.covars_[iweight][np.ix_(impute_sample_nan_idxs,impute_sample_nan_idxs)])
-  if impute_sample_nan_idxs:
-    impran= scipy.stats.multivariate_normal.rvs(g.means_[iweight][impute_sample_nan_idxs],g.covars_[iweight][impute_sample_nan_idxs]*np.eye(len(impute_sample_nan_idxs)))
-    #impran= scipy.stats.multivariate_normal.rvs(g.means_[iweight][impute_sample_nan_idxs],g.covars_[iweight][np.ix_(impute_sample_nan_idxs,impute_sample_nan_idxs)])
-  else:
-    impran=1
-  #print impran
-  #print >> log, "pref norm =", g.means_[iweight][impute_sample_num_idxs]
-  #print >> log, "pref cov =",g.covars_[iweight][impute_sample_num_idxs]
-  #print >> log, "pref cov =",g.covars_[iweight][np.ix_(impute_sample_num_idxs,impute_sample_num_idxs)]
-  #print "num =",impute_sample[impute_sample_num_idxs]
-  #print "means = ",g.means_[iweight][impute_sample_num_idxs]
-  #print "covs = ",g.covars_[iweight][np.ix_(impute_sample_num_idxs,impute_sample_num_idxs)]
-  #pref = scipy.stats.multivariate_normal.pdf(impute_sample[impute_sample_num_idxs],g.means_[iweight][impute_sample_num_idxs],g.covars_[iweight][impute_sample_num_idxs]*np.eye(len(impute_sample_num_idxs)))
-  #prefs+=[pref]
-  #print pref
-  #impran*= pref
-  imps+=[impran]
-  print >>log,isodx," imputed = ",impran
-#print >>log,"sorted(pref): ",np.sort(prefs)
-#print "sort(pref): ",np.sort(prefs)[-10:]
-
-#print imps
+for igmm in range (3):
+  g.fit(fit_data)
+  ### Decide from which gaussian to sample by sampling from weights
+  decide_node_sample = random.random()
+  iweight = 0
+  running_sum = 0
+  while decide_node_sample>running_sum:
+    running_sum+=g.weights_[iweight]
+    iweight+=1
+  iweight-=1
+  ### sample nans for each sample
+  for isodx, impute_sample in data.ix[:,:].iterrows():
+    nan_means = []
+    nan_covars = []
+    val_means = []
+    val_covars = []
+    ### separate the vector into known and unknown.
+    if nan_not_counted:
+      impute_sample_nan_idxs = []
+      impute_sample_num_idxs = []
+      for idx,i in enumerate(impute_sample):
+        if np.isnan(i):
+          impute_sample_nan_idxs+=[idx]
+        else:
+          impute_sample_num_idxs+=[idx]
+      print >>log,impute_sample_nan_idxs ,impute_sample_num_idxs
+      nans_and_nums += [impute_sample_nan_idxs,impute_sample_num_idxs]
+      nan_not_counted=False
+    if impute_sample_nan_idxs:
+      impran= scipy.stats.multivariate_normal.rvs(g.means_[iweight][impute_sample_nan_idxs],g.covars_[iweight][impute_sample_nan_idxs]*np.eye(len(impute_sample_nan_idxs)))
+      print "total nans = ",len([j for j in impute_sample if np.isnan(j)])
+      for idx,i in enumerate(impute_sample[:]):
+        if np.isnan(i):
+          print len(impran)
+          impute_sample[idx]=impran[0]
+          impran=impran[1:]
+      fit_data.iloc[isodx]=impute_sample
+  #print igmm,fit_data.ix[:0,:5].values
 
 
-
-### calculate the coefficient from the known
-
-## sample multivariate normal with uknown.
-
-
-#tcols = {}
-#nans ={}
-#combine = []
-#for i in self.cols:
-  #d = self.col_to_list(data[i])
-  #col_nans = []
-  #col_nans_value = []
-  #nonan = d.dropna()
-  ##print "median", median
-  #for j,v in enumerate(d):
-    ##print j
-    #if d[j] is np.nan:
-      #col_nans += [1]
-    #else:
-      #col_nans += [0]
-
-#train_filename = (os.path.splitext(os.path.basename(sys.argv[3]))[0]+"_imm.csv")
-#train_columns = train.columns
-#train = pd.DataFrame(train_imp, columns=train_columns)
-#train = pd.concat([train,data_train_in.ix[:,'target']],axis=1)
-#train.to_csv(train_filename,index=0)
-#test_filename = (os.path.splitext(os.path.basename(sys.argv[2]))[0]+"_imm.csv")
-#test_columns = test.columns
-#test = pd.DataFrame(test_imp, columns=test_columns)
-#if 'target' in data_test_in:
-    #test = pd.concat([test,data_test_in.ix[:,'target']],axis=1)
-#test.to_csv(test_filename,index=0)
+data_filename = (os.path.splitext(os.path.basename(sys.argv[2]))[0]+"_imgmm.csv")
+fit_data.to_csv(data_filename,index=0)
 
 #print train_filename, test_filename
 
