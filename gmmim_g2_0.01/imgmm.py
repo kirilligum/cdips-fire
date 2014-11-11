5c7652310726193ad99284ecb648353c880787cb
Kirill Igumenshchev <kirill@berkeley.edu>
2014-11-10 22:11:25 -0800

    added reduce samples, plca, and gmm imputation results

---
+++ gmmim_g2_0.01/imgmm.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import scipy
+from sklearn.preprocessing import Imputer
+from sklearn import mixture
+import time
+import sys
+import os
+import random
+import itertools
+
+class impute_gmm:
+  def __init__(self,data):
+    self.data = data
+    self.nans_and_nums=[]
+    self.mean_ranges = pd.DataFrame(columns=self.data.columns)
+    self.g = mixture.GMM()
+    return
+  def fit(self,fit_data,min_covar=0.01, n_components=1):
+    self.g = mixture.GMM(n_components=n_components, min_covar=min_covar)
+    self.g.fit(fit_data)
+  def transform(self,fit_data):
+    decide_node_sample = random.random()
+    iweight = 0
+    running_sum = 0
+    while decide_node_sample>running_sum:
+      running_sum+=self.g.weights_[iweight]
+      iweight+=1
+    iweight-=1
+    #self.mean_ranges.loc[len(self.mean_ranges)] = self.g.means_[iweight] #prints out mean ranges for each gmm run
+    print >>log,fit_data.shape, self.g.means_[iweight].shape,self.mean_ranges.shape
+    ### sample nans for each sample
+    for isodx, impute_sample in fit_data.ix[:,:].iterrows():
+      ### separate the vector into known and unknown.
+      impute_sample_nan_idxs = []
+      #impute_sample_num_idxs = []
+      for idx,i in enumerate(impute_sample):
+        if np.isnan(i):
+          impute_sample_nan_idxs+=[idx]
+        #else:
+          #impute_sample_num_idxs+=[idx]
+      if impute_sample_nan_idxs:
+        impran= scipy.stats.multivariate_normal.rvs(self.g.means_[iweight][impute_sample_nan_idxs],self.g.covars_[iweight][impute_sample_nan_idxs]*np.eye(len(impute_sample_nan_idxs)))
+        for idx,i in enumerate(impute_sample[:]):
+          if np.isnan(i):
+            impute_sample[idx]=impran[0]
+            impran=impran[1:]
+        fit_data.iloc[isodx]=impute_sample
+    return fit_data
+
+log = open(os.path.splitext(sys.argv[0])[0]+'.log','wt')
+
+data_train_in = pd.read_csv(sys.argv[1])
+data_test_in = pd.read_csv(sys.argv[2])
+train = data_train_in.drop('target',axis=1)
+if 'target' in data_test_in:
+  test = data_test_in.drop('target',axis=1)
+else:
+  test = data_test_in
+data = pd.concat([train,test])
+print >> log,  train.shape, "  ", test.shape ,"  " , data.shape
+
+#data = pd.read_csv(sys.argv[1])
+miss = data.apply(lambda x : x.apply(lambda y: np.isnan(y))) # T/F data frame of missing values
+imp_mean = Imputer(missing_values=np.nan, strategy='mean', axis=0)
+fit_data = pd.DataFrame(imp_mean.fit_transform(data),columns=data.columns)
+#np.set_printoptions(formatter={'float': '{: 0.5f}'.format})
+ig = impute_gmm(data)
+for igmm_min_covar in [0.01]:
+  ig.fit(fit_data,igmm_min_covar,2)
+  train_imp = ig.transform(train)
+  test_imp = ig.transform(test)
+
+id_train = data_train_in.ix[:,'id']
+id_test = data_test_in.ix[:,'id']
+train_imp = pd.concat([id_train,train_imp],axis=1)
+test_imp = pd.concat([id_test,test_imp],axis=1)
+
+train_filename = (os.path.splitext(os.path.basename(sys.argv[1]))[0]+"_gmmim.csv")
+train = pd.concat([train,data_train_in.ix[:,'target']],axis=1)
+train.to_csv(train_filename,index=0)
+test_filename = (os.path.splitext(os.path.basename(sys.argv[2]))[0]+"_gmmim.csv")
+if 'target' in data_test_in:
+    test = pd.concat([test,data_test_in.ix[:,'target']],axis=1)
+test.to_csv(test_filename,index=0)
+
+print train_filename, test_filename
+
+log.close()
+
+
---
+++ reduce_samples/xfc.py
@@ -0,0 +1,86 @@
+#!/bin/python
+import pandas as pd
+import numpy as np
+import time
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+#from sklearn.metrics import accuracy_score
+#from sklearn.metrics import r2_score
+#from gini import normalized_weighted_gini
+#import random
+import sys
+import os
+
+
+log = open(os.path.splitext(sys.argv[0])[0]+'.log','wt')
+
+data_train = pd.read_csv(sys.argv[1])
+print >> log,  data_train.shape
+
+start = time.clock()
+#print >> log, data_train.columns
+target_train = data_train['target']
+#print >> log, target_train
+data_train_zeros = data_train[data_train['target']==0]
+data_train_zeros_nt = data_train_zeros.drop('target',axis=1)
+data_train_nt = data_train.drop('target',axis=1)
+
+#xfr = ExtraTreesClassifier(n_estimators =1000,n_jobs=-1,oob_score=True,max_depth=7)
+xfr = ExtraTreesClassifier(n_estimators =10000,n_jobs=-1,max_depth=7)
+#xfr = RandomForestClassifier (n_estimators =10000,n_jobs=-1,max_depth=7)
+print >> log, target_train.shape
+xfr = xfr.fit(data_train_nt,[0 if x==0 else 1 for x in target_train])
+
+#with open('model.txt','wt') as f:
+  #print >> f, xfr
+#with open('estimators_.txt','wt') as f:
+  ##f.write(xfr.estimators_)
+  #print >> f, xfr.estimators_
+#with open('feature_importances_.txt','wt') as f:
+  #print >> f, xfr.feature_importances_
+#with open('oob_score_.txt','wt') as f:
+  #print >> f, xfr.oob_score_
+#with open('oob_decision_function_.txt','wt') as f:
+  #print >> f, xfr.oob_decision_function_
+
+
+predcted_prob  = pd.concat([data_train,pd.DataFrame(xfr.predict(data_train_nt),columns=['predicted']), pd.DataFrame( xfr.predict_proba(data_train_nt),columns=['proba0','proba1'])],axis=1).sort(['target','proba0'],ascending=[False,False])
+
+#print "false positives", len(predcted_prob[(predcted_prob['target']==0)].any() and predcted_prob[(predcted_prob['predicted']==1)].any())
+print >> log, "false positives", len(predcted_prob[(predcted_prob['target']==0) & (predcted_prob['predicted']==1)])
+print >> log, "false negative", len(predcted_prob[(predcted_prob['target']!=0) & (predcted_prob['predicted']==0)])
+print >> log, "true positives", len(predcted_prob[(predcted_prob['target']!=0) & (predcted_prob['predicted']==1)])
+print >> log, "true negative", len(predcted_prob[(predcted_prob['target']==0) & (predcted_prob['predicted']==0)])
+
+print >> log, "nonzeros = ", len(target_train[target_train!=0])
+print >> log, "zeros = ", len(target_train[target_train==0])
+
+#print type(proba),proba.shape,proba
+#np.savetxt("proba.csv",proba,delimiter=", ")
+#log_proba = xfr.predict_log_proba(data_train_nt)
+
+
+#if 'target_train' in locals():
+  #score = xfr.score(data_train_nt,[0 if x is 0 else 1 for x in target_train])
+end = time.clock()
+
+#if 'target_train' in locals():
+  #target_train.columns = ['true_target']
+  #outdf = pd.concat([data_train_nt.ix[:,'id'].astype(int),pd.DataFrame(predict_loc_classifier,columns=['pred_target'])],axis=1)
+  #outdf = pd.concat([outdf,pd.DataFrame(proba,columns=['proba0','proba1'])],axis=1)
+  #outdf = pd.concat([outdf,pd.DataFrame(log_proba,columns=['log_proba'])],axis=1)
+  #outdf = pd.concat([outdf,target_train],axis=1)
+  #outdf = pd.concat([data_train_nt.ix[:,'id'].astype(int),pd.DataFrame(predict_loc_classifier,columns=['pred_target']),pd.DataFrame(proba,columns=['proba']),pd.DataFrame(log_proba,columns=['log_proba']),target_train],axis=1)
+#else:
+  #outdf = pd.concat([data_train_nt.ix[:,'id'].astype(int),pd.DataFrame(predict_loc_classifier,columns=['pred_target'])],axis=1)
+  #outdf = pd.concat([data_train_nt.ix[:,'id'].astype(int),pd.DataFrame(predict_loc_classifier,columns=['target']),pd.DataFrame(proba,columns=['proba']),pd.DataFrame(log_proba,columns=['log_proba'])],axis=1)
+
+out_filename = (os.path.splitext(os.path.basename(sys.argv[1]))[0]+"_prob.csv")
+#predcted_prob.to_csv("predproba.csv",index=0)
+predcted_prob.to_csv(out_filename,index=0)
+#if 'target_train' in locals():
+  #print out_filename, score
+#else:
+  #print out_filename
+
+log.close()
